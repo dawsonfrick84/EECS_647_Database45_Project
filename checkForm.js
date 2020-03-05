5c1b23d16bb6c41f58478f5caa3458e1258ff248
Dawson Frick <d289f187@ku.edu>
2020-03-05 12:28:24 -0600

    Update to check form for correct submissions

---
+++ checkForm.js
@@ -0,0 +1,40 @@
+function checkSubmit(){
+    var valid=false;
+    if(document.getElementById("user").value.length>=5){
+      if(document.getElementById("pass").value.length>=5){
+        if(document.getElementById("pass").value==document.getElementById("conf_pass").value){
+          valid=true;
+        }
+      }
+    }
+
+    if(valid==true){
+      return true;
+    }
+    else if(document.getElementById("user").value.length<5){
+      alert("Username must be five or more characters")
+      return false;
+      returnToPreviousPage();
+    }
+    else if(document.getElementById("pass").value.length<5){
+      alert("Password must be five or more characters")
+      return false;
+      returnToPreviousPage();
+    }
+    else if(document.getElementById("pass").value!=document.getElementById("conf_pass").value){
+      alert("Passwords do not match")
+      return false;
+      returnToPreviousPage();
+    }
+    else{
+      alert("Please enter all required fields")
+      return false;
+      returnToPreviousPage();
+    }
+}
+
+function reset(){
+      document.getElementById("user").value = "";
+      document.getElementById("pass").value = "";
+      document.getElementById("conf_pass").value = "";
+}
