6fad4aef17f4d61d62d9156deab7fde3624da524
Dawson Frick <d289f187@ku.edu>
2020-04-16 13:46:23 -0500

    Update purchasing items

--- buyItems.php
+++ buyItems.php
@@ -1,22 +1,39 @@
 <?php
+$mysqli = new mysqli("mysql.eecs.ku.edu", "dawsonfrick84", "aij4eeph", "dawsonfrick84");
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s<br>", $mysqli->connect_error);
+    exit();
+}
+else{
+  //printf("Connection to mySQL successful!");
+}
+$query = "SELECT item_id, name, stock, price, rating, picture, description FROM Items ORDER by item_id";
 echo"
 <html>
     <head>
       <title>Store</title>
     </head>
 <body>
-<link rel='stylesheet' type='text/css' href='style.css'>
+
 <script src='checkForm.js'></script>
 <center><h1><center>Welcome to the School Supplies Store</center></h1></center><br>
 <center><h3><center>Check out our selection of school supplies and textbooks</center></h3></center><br>
 <div id='items'>
-  <form action='showOrder.php' method='post' onsubmit='return checkSubmit();'>
-    <center><figure>
-          <center><b>Item1</b></center>
-          <img class='item' src='' width='200' height='200'><br>
-            <input type='checkbox' id='1' name='1' value='item1'>$3.99<br>
-            Quantity: <input type='number' id='q1' name='q1' value='q1' min='1' max='99' size=3><br>
-    </figure></center>
+  <form action='showOrder.php' method='post' onsubmit='return checkSubmit();'>";
+
+    if ($result = $mysqli->query($query)) {
+      //printf("Connection success<br>Passed in username: %s<br>", $user);
+        while ($row = $result->fetch_assoc()) {
+          echo"
+          <center><figure>
+           <center><b>Item ".$row["item_id"].": ".$row["name"]." </b></center>
+            <img class='item' src='".$row["picture"]."' width='200' height='200'><br>
+              <input type='checkbox' id='".$row["item_id"]."' name='".$row["name"]."' value='".$row["item_id"]."'>$".$row["price"]."<br>
+              Quantity: <input type='number' id='q".$row["item_id"]."' name='q".$row["name"]."' value='q".$row["item_id"]."' min='1' max='".$row["stock"]."' size=3><br>
+              </figure></center>";
+          }
+    }
+    echo"
     <center><figure>
         <form action='showOrder.php' method='post'>
           <center><b>item2</b></center>
@@ -27,7 +44,7 @@ echo"
   </div>
   <div class='clear'></div><br><br><br>
   <p><b>Checkout</b></p>
-  <b>Username (email):&nbsp&nbsp</b><input type='email' id='user' name='user'><br><br>
+  <b>Username:&nbsp&nbsp</b><input type='text' id='user' name='user'><br><br>
   <b>Password:&nbsp&nbsp</b><input type='password' id='pass' name='pass'><br><br><br>
   <p><b>Shipping Options:</b></p>
   <input type='radio' id='shipping1' name='shipping' value='free' unchecked>Free 5-Day Shipping<br>
---
+++ showOrder.php
@@ -0,0 +1,105 @@
+<?php
+$mysqli = new mysqli("mysql.eecs.ku.edu", "dawsonfrick84", "aij4eeph", "dawsonfrick84");
+$user=$_POST["user"];
+$email=$_POST["email"];
+$pass=$_POST["pass"];
+$shipping=$_POST["shipping"];
+$cost = 0;
+  echo"
+  <html>
+      <head>
+        <title>Order Created</title>
+      </head>
+  <body>
+  ";
+  if ($mysqli->connect_errno) {
+      printf("Connect failed: %s<br>", $mysqli->connect_error);
+      exit();
+  }
+  else{
+    //printf("Connection to mySQL successful!");
+  }
+
+  $query = "SELECT username, password FROM Users ORDER by username";
+  $found=FALSE;
+
+  if ($result = $mysqli->query($query)) {
+    //printf("Connection success<br>Passed in username: %s<br>", $user);
+      while ($row = $result->fetch_assoc()) {
+          if ($row["username"]==$user&&password_verify($pass, $row["password"]) ){
+            $found=TRUE;
+          }
+      }
+      if ($found==FALSE){
+        echo"<h1>User not found in the database...</h1>
+        <h2><a href='index.html'>Create a New User</a></h2>
+        <h1>Or maybe you entered the wrong password</h1>
+        <h2><a href='javascript:history.back()'>Go Back</a></h2>
+        ";
+      }
+      else {
+        //printf("User and password exists!");
+        echo"<html>
+        <head>
+          <title>Receipt</title>
+        <head>
+        <body>
+          <h1>Order Confirmed!</h1>
+          <h2>Order made by user:  " . $user . "!</h2>
+          <h1>Receipt</h1>
+
+        <table cellspacing='7.5'>
+        <tr>
+        <th>&nbsp&nbsp&nbsp</th>
+        <th>Quantity</th>
+        <th>Cost Per Item</th>
+        <th>Sub Total</th>
+        </tr>
+        ";
+        echo"<tr><th>Shipping</th>";
+
+        if($shipping=="free"){
+          echo"
+          <td colspan='2'>5-Day Shipping</td>
+          <td>0.00</td>
+          </tr>";
+        }
+        if($shipping=="three"){
+          echo"
+          <td colspan='2'>3-Day Shipping</td>
+          <td>3.00</td>
+          </tr>";
+          $cost=$cost+3.00;
+        }
+        if($shipping=="overnight"){
+          echo"
+          <td colspan='2'>Overnight Shipping&nbsp&nbsp&nbsp&nbsp</td>
+          <td>25.00</td>
+          </tr>";
+          $cost=$cost+25.00;
+        }
+
+        echo"<tr><td colspan='3'><b>TOTAL COST:&nbsp&nbsp&nbsp&nbsp</b></td>
+        <td><b>&nbsp&nbsp&nbsp$" . $cost . "</b></td>
+        </tr>
+        </table>
+        <br><br><button onclick='printReceipt()'><b>Print Receipt</b></button>
+        <script>
+        function printReceipt() {
+          window.print();
+        }
+        </script>
+        <br><br>
+        <h3><a href='index.html'>Return to Home Page</a></h3>
+        ";
+      }
+
+
+      $result->free();
+  }
+
+
+
+  $mysqli->close();
+
+?>
